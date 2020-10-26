cb946c77e79e66b430be54d3f037d9ae7e242de4
makmakkerti <makmakkerti007@gmail.com>
2020-10-26 18:51:59 +0200

    Async added instead promise chaining to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable no-console */
 const express = require('express');
 require('./db/mongoose');
@@ -9,24 +10,72 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user.save().then(() => {
-    res.send(user);
-  }).catch((err) => {
-    res.status(400).send(err);
-  });
+  await user.save();
+  try {
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
-app.post('/tasks', (req, res) => {
-  const task = new Task(req.body);
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-  task.save().then(() => {
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).send('User was not found');
+    }
+    return res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findById(id);
+    console.log(task);
+    if (!task) {
+      return res.status(404).send('Task was not found');
+    }
     res.send(task);
-  }).catch((err) => {
-    res.status(400).send(err);
-  });
+  } catch (e) {
+    res.status(500).send();
+  }
 });
 
 app.listen(port, () => {
